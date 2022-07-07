68bfdc3a296b44fe2b752b7fb28086f777abef3b
santannaizadora <santannaizadora@gmail.com>
2022-07-07 10:44:48 -0300

    refactor: battle service

--- src/services/battleService.ts
+++ src/services/battleService.ts
@@ -2,57 +2,49 @@ import axios from "axios";
 import { battleRepository } from "../repositories/battleRepository.js";
 
 const saveBattle = async (winner: string, loser: string, draw: boolean, firstUser: string, secondUser: string) => {
-    const firstUserFighter = await battleRepository.findFighterByUserName(firstUser);
-    const secondUserFighter = await battleRepository.findFighterByUserName(secondUser);
-    if (!firstUserFighter) {
-      await battleRepository.createFighter(firstUser);
-    }
-    if (!secondUserFighter) {
-      await battleRepository.createFighter(secondUser);
-    }
-    if (draw) {
-      await battleRepository.updateDraws(firstUser);
-      await battleRepository.updateDraws(secondUser);
-    } else {
-      await battleRepository.updateWins(winner);
-      await battleRepository.updateLosses(loser);
-    }
+  const firstUserFighter = await battleRepository.findFighterByUserName(firstUser);
+  const secondUserFighter = await battleRepository.findFighterByUserName(secondUser);
+
+  !firstUserFighter ? await battleRepository.createFighter(firstUser) : null;
+  !secondUserFighter ? await battleRepository.createFighter(secondUser) : null;
+
+  if (draw) {
+    await battleRepository.updateDraws(firstUser);
+    await battleRepository.updateDraws(secondUser);
+  } else {
+    await battleRepository.updateWins(winner);
+    await battleRepository.updateLosses(loser);
+  }
 }
 
 const battle = async (firstUser: string, secondUser: string) => {
-  try {
-    const firstUserResponse = await axios.get(`https://api.github.com/users/${firstUser}/repos`);
-    const secondUserResponse = await axios.get(`https://api.github.com/users/${secondUser}/repos`);
-    const firstUserStars = firstUserResponse.data.reduce((acc: Number, repo: any) => acc + repo.stargazers_count, 0);
-    const secondUserStars = secondUserResponse.data.reduce((acc: Number, repo: any) => acc + repo.stargazers_count, 0);
-    
-    let winner: string = null;
-    let loser: string = null;
-    let draw = false;
+  const firstUserResponse = axios.get(`https://api.github.com/users/${firstUser}/repos`);
+  const secondUserResponse = axios.get(`https://api.github.com/users/${secondUser}/repos`);
 
-    if (firstUserStars > secondUserStars) {
-      winner = firstUser;
-      loser = secondUser;
-    } else if (firstUserStars < secondUserStars) {
-      winner = secondUser;
-      loser = firstUser;
-    } else {
-      draw = true;
-    }
+  return await axios.all([firstUserResponse, secondUserResponse])
+    .then(axios.spread(async (firstUserResponse, secondUserResponse) => {
+      const firstUserStars = firstUserResponse.data.reduce((acc: Number, repo: any) => acc + repo.stargazers_count, 0);
+      const secondUserStars = secondUserResponse.data.reduce((acc: Number, repo: any) => acc + repo.stargazers_count, 0);
 
-    await saveBattle(winner, loser, draw, firstUser, secondUser);
+      let winner: string = null;
+      let loser: string = null;
+      let draw = false;
 
-    return {winner, loser, draw};
+      firstUserStars > secondUserStars ? winner = firstUser : secondUserStars > firstUserStars ? winner = secondUser : draw = true;
+      firstUserStars < secondUserStars ? loser = firstUser : secondUserStars < firstUserStars ? loser = secondUser : draw = true;
 
-  } catch (error) {
-    if (error.response.status === 404) {
-      throw {
-        type: "not_found",
-        message:
-          "At least one of the fighters does not exist, please try again",
-      };
-    }
-  }
+      await saveBattle(winner, loser, draw, firstUser, secondUser);
+
+      return {winner, loser, draw};
+    }))
+    .catch(error => {
+      if (error.response.status === 404) {
+        throw {
+          type: "not_found",
+          message: "At least one of the fighters does not exist, please try again",
+        };
+      }
+    });
 };
 
 export const battleService = {
